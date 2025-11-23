bb8c4098b9bcc5613ef7ded89372dcd7d1c425f8
Qwerty7310 <daniyl.2004@yandex.ru>
2025-11-23 19:32:55 +0300

    add CLI for client

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,1 +1,63 @@
-package client
+package main
+
+import (
+	"bufio"
+	"chat-grpc/api/pb"
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/google/uuid"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("failed to connect: %v", err)
+	}
+	defer conn.Close()
+
+	c := pb.NewChatServiceClient(conn)
+
+	user := &pb.User{
+		Id:       uuid.New().String(),
+		Username: "test-user",
+	}
+
+	// Создаем поток
+	stream, err := c.ChatStream(context.Background(), user)
+	if err != nil {
+		log.Fatalf("failed to call ChatStream: %v", err)
+	}
+
+	go func() {
+		for {
+			msg, err := stream.Recv()
+			if err != nil {
+				log.Println("stream recv err:", err)
+				return
+			}
+			fmt.Printf("[%s] %s\n", msg.Username, msg.Text)
+		}
+	}()
+
+	// Отправка сообщений с консоли
+	fmt.Println("Connected. Type messages:")
+
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		text, _ := reader.ReadString('\n')
+
+		_, err := c.SendMessage(context.Background(), &pb.MessageToServer{
+			UserId:   user.Id,
+			Username: user.Username,
+			Text:     text,
+		})
+		if err != nil {
+			log.Println("send error:", err)
+		}
+	}
+
+}
