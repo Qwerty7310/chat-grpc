25b3f683348dd1db131e0ccb17f84dc0e04254a9
Qwerty7310 <daniyl.2004@yandex.ru>
2025-11-23 19:05:59 +0300

    add main.go for server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,26 @@
-package server
+package main
+
+import (
+	"chat-grpc/api/pb"
+	"chat-grpc/internal/server"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterChatServiceServer(s, server.NewChatService())
+
+	log.Println("gRPC server listening on :50051")
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
